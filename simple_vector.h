efb3de312a516a0895155c5823226556d6d72ebb
Artem <47495212+art25062025@users.noreply.github.com>
2020-06-24 23:02:15 +1000

    Update simple_vector.h

--- simple_vector.h
+++ simple_vector.h
@@ -6,10 +6,18 @@
 template <typename T>
 class SimpleVector {
 public:
-  SimpleVector();
+  SimpleVector(){
+	  data = nullptr;
+	  //_size = 0;
+	  capacity = 0;
+	  _end = data;
+
+  }
   explicit SimpleVector(size_t size){
 	  data = new T[size];
-	  end_ = data + size;
+	  //_size = size;
+	  _end = data + size;
+	  capacity = size;
   }
   ~SimpleVector(){
 	  delete[] data;
@@ -23,17 +31,56 @@ public:
 	  return data;
   }
   T* end(){
-	  return end_;
+	  return _end;
   }
 
   size_t Size() const{
-	  return end_ - data;
+	  return _end - data;
   }
-  size_t Capacity() const;
-  void PushBack(const T& value);
+  size_t Capacity() const{
+	  return capacity;
+  }
+
+
+  void PushBack(const T& value){
+	  if(isFirst){
+		  capacity = 1;
+		  data = new T[capacity];
+		  *data = value;
+		  _end = data + 1;
+		 isFirst = false;
+		 return;
+
+	  }else{
+		  if(Size() == Capacity()){
+			  capacity = 2 * Capacity();
+			  int t = Size();
+			  T* tp = data;
+			  T* data = new T[capacity];
+			  for(size_t i = 0; i < Size(); ++i){
+				  *(data + i) = *(tp + i);
+			  }
+
+			  _end = data + t;
+			  *(_end + 1) = value;
+			  ++ _end;
+
+		  }else{
+			  *(_end + 1) = value;
+			  ++_end;
+
+		  }
+
+	  }
+
+  }
+
 
 private:
   // Добавьте поля для хранения данных вектора
   T* data;
-  T* end_;
+  T* _end;
+  //size_t _size;
+  size_t capacity;
+  bool isFirst = true;
 };
